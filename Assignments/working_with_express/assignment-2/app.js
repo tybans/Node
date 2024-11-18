7dea8ccd4034666c1f6fb65caf8700ff1bcea85a
Taiyab Ansari <taiyab0523@gmail.com>
2024-11-18 20:22:53 +0530

    Assignment

--- Assignments/working_with_express/assignment-2/app.js
+++ Assignments/working_with_express/assignment-2/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const path = require('path')
 const mainRoute = require('./routes/index')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(mainRoute)
 
 app.listen(4000)
