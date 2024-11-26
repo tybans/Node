2cd7c3af20f9957eec30a6822c966d7ec6ebe311
Taiyab Ansari <taiyab0523@gmail.com>
2024-11-26 08:49:17 +0530

    The MVC Pattern

---
+++ Practice/7_the_model_view_controller/controllers/404.js
@@ -0,0 +1,5 @@
+exports.get404 = (req, res, next) => {
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    //   res.status(404).render("404");
+    res.render("404", { docTitle: "Page Not Found" });
+  }
